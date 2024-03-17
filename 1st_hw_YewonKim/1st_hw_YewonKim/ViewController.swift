f180ee79ddaae5cb6eff8960b91b2aa883e62bf2
Yewon Kim <72730302+noweymik@users.noreply.github.com>
2024-03-17 20:53:35 +0900

    feat: calculator UI

---
+++ 1st_hw_YewonKim/1st_hw_YewonKim/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  1st_hw_YewonKim
+//
+//  Created by yewoni on 3/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+//@IBDesignable extension UIView {
+//    @IBInspectable var cornerRadius: CGFloat {
+//        get { return layer.cornerRadius }
+//        set {
+//              layer.cornerRadius = newValue
+//
+//              // If masksToBounds is true, subviews will be
+//              // clipped to the rounded corners.
+//              layer.masksToBounds = (newValue > 0)
+//        }
+//    }
+//}
